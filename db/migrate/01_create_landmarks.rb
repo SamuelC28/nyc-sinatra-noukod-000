4f011a4a0a0a78ea202777cbc0c2bef732c1c601
Samuel Cornet <samuel@noukod.com>
2019-01-14 16:49:45 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,7 @@
 class CreateLandmarks < ActiveRecord::Migration
-  raise 'Write CreateLandmarks migration here'
+  # raise 'Write CreateLandmarks migration here'
+   t.string :name
+   t.belongs_to :figure
+   t.integer :year_completed
 end
+s
