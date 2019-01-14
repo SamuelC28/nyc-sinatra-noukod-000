712c2ea3b6dd175393f4dc6cb340f67f078c4a7e
Samuel Cornet <samuel@noukod.com>
2019-01-14 18:24:33 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -28,7 +28,26 @@ class FiguresController < ApplicationController
     @figure = Figure.find params[:id]
     erb :'figures/show'
   end
-  # post '/figures/new' do
-  #
-  # end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find params[:id]
+    @titles = Title.all
+    @landmarks = Landmark.all
+
+    erb :'figures/edit'
+  end
+
+  patch '/figures/:id' do
+    figure = params[:figure]
+    title = params[:title]
+    landmark = params[:landmark]
+
+    @figure = Figure.find params[:id]
+    @figure.update figure
+    # @figure.landmark_ids =
+    @figure.titles.find_or_create_by(name: title[:name])
+    @figure.landmarks.find_or_create_by(name: landmark[:name])
+
+    redirect "/figures/#{@figure.id}"
+  end
 end
