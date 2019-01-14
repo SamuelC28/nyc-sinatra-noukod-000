8ad6824b8a9032c96e68640220fa8082fd379783
Samuel Cornet <samuel@noukod.com>
2019-01-14 17:21:08 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,14 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures/new' do
+    
+  end
+
+  get 'figures'
+
+  end 
+
+  post '/figures/new' do
+    
+  end
 end
